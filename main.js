c454aef1c14169eeb0d7306527ec6fbd0c2b92a1
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-08-15 20:13:23 +0300

    Task 1

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class Student {
+    constructor(enrollee) {
+        this.id = Student.id++;
+        this.name = enrollee.name;
+        this.surname = enrollee.surname;
+        this.ratingPoint = enrollee.ratingPoint;
+        this.schoolPoint = enrollee.schoolPoint;
+        this.isSelfPayment = Student.isSelfPayment(this, this.ratingPoint);
+    }
+
+    static id = 1;
+    static listOfStudents = [];
+    static isSelfPayment(obj, point) {
+        if (point >= 800) {
+            this.listOfStudents.push(obj);
+            this.listOfStudents.sort((a,b) => {
+              if (a.ratingPoint === b.ratingPoint) {
+                return b.schoolPoint - a.schoolPoint;
+              } else {
+                return b.ratingPoint - a.ratingPoint;
+              }    
+          });
+        } else if (point < 800) {
+            this.listOfStudents.push(obj);
+        }
+        for (let i = 0; i < this.listOfStudents.length; i++) {
+          if (i <= 4) {
+          this.listOfStudents[i].isSelfPayment = false;
+          } else {
+          this.listOfStudents[i].isSelfPayment = true;
+          }
+        }
+    }
+}
+
+for (let key in studentArr) {
+    new Student(studentArr[key]);
+}
+console.log(Student.listOfStudents);
+
