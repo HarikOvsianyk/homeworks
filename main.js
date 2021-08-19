759b8dec7e21fc6e4ab96dd2e3295f97a44ffb22
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-08-19 10:49:47 +0300

    Task 2

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Task 1
+
 const arr = ['Vasya', 'Petya', 'Alexey'];
 
 function removeUser(arr, index) {
@@ -8,5 +10,18 @@ function removeUser(arr, index) {
 removeUser(arr, 2);
 console.log(arr);
 
+//Task 2
+
+const obj = { name: 'Vasya', age: 1};
+
+function getAllKeys(obj) {
+    let arr = [];
+    for (let key in obj) {
+        arr.push(key);
+    }
+    return arr;
+}
+
+console.log(getAllKeys(obj));
 
 
