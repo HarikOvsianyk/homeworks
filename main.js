9c3439fe988266dfd52470b69c02627f337c744f
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-08-23 13:51:04 +0300

    Task 3

--- main.js
+++ main.js
@@ -6,17 +6,44 @@ function searchCandidatesByPhoneNumber(phone) {
     phone = phone.match(/\d/gi).join('');
     return condidateArr.filter(el => el.phone.match(/\d/gi).join('').includes(phone));
 }
-console.log(searchCandidatesByPhoneNumber('43'));
+console.log(searchCandidatesByPhoneNumber('+1 (803) 433-2863'));
 
 //Task 2
 
 function getCandidateById(id) {
-    let condidate = condidateArr.find(el => el._id == id);
+    const condidate = condidateArr.find(el => el._id == id);
     condidate.registered = condidate.registered.slice(0,10).split('-').reverse().join('/');
     return condidate;
 }
 
 console.log(getCandidateById("5e216bc9a6059760578aefa4")); 
+
+//Task 3
+
+function sortCandidatesArr(sortBy) {
+    const up   = 'asc',
+          down = 'desc';
+    condidateArr.forEach(el => {
+        let balance = el.balance.match(/\d/gi).join('');
+        el.balance = balance;
+    });
+
+    if( sortBy == up) {
+        condidateArr.sort((a,b) => {
+            return a.balance - b.balance
+        })
+        return condidateArr;
+    } else if (sortBy == down) {
+        condidateArr.sort((a,b) => {
+           return b.balance - a.balance
+        })
+        return condidateArr;
+    } else {
+        return condidateArr;
+    }
+}
+
+console.log(sortCandidatesArr('asc'));
 
 
 
