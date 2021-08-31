bd56f6ae9573e8cf4f5789201d1afa6ec3440f1f
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-08-31 13:33:41 +0300

    Com

--- main.js
+++ main.js
@@ -31,9 +31,8 @@ console.log(getCandidateById("5e216bc9a6059760578aefa4"));
 function sortCandidatesArr(sortBy) {
     const up   = 'asc',
           down = 'desc';
-    condidateArr.forEach(el => {
-        el.balance = el.balance.match(/\d/gi).join('');
-    });
+    condidateArr.forEach(el => 
+        el.balance = el.balance.match(/\d/gi).join(''));
 
     if( sortBy == up) {
         condidateArr.sort((a,b) => {
