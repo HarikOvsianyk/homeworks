b9de1c6a1775f1fe7e158b2728b99222eec1cd78
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-09-12 14:31:11 +0300

    N3 working with arrays

--- js/script.js
+++ js/script.js
@@ -2,36 +2,34 @@
 
 let arrayEvents = [
     {start: 0, duration:15, title: "Exercise"},
-    {start: 25, duration:15, title: "Exercise"},
-    {start: 30, duration:15, title: "Exercise"},
-    {start: 60, duration:15, title: "Exercise"},
-    {start: 100, duration:15, title: "Exercise"},
-    {start: 180, duration:15, title: "Exercise"},
-    {start: 360, duration:15, title: "Exercise"},
-    {start: 370, duration:15, title: "Exercise"},
-    {start: 405, duration:15, title: "Exercise"},
+    {start: 25, duration:15, title: "Travel to work"},
+    {start: 30, duration:15, title: "Plan day"},
+    {start: 60, duration:15, title: "Review yesterday`s commits"},
+    {start: 100, duration:15, title: "Code review"},
+    {start: 180, duration:15, title: "Have lunch with John"},
+    {start: 360, duration:15, title: "Skype call"},
+    {start: 370, duration:15, title: "Follow up with designer"},
+    {start: 405, duration:15, title: "Push up branch"},
 ];
 
+
 const timeSlot = document.querySelectorAll('.card');
-let pageEvent = document.querySelector('.card');
 let cards = Array.from(timeSlot).map(item => item.id);
 
-arrayEvents.map(item => {
-    cards.map(i => {
+arrayEvents.forEach(item => {
+    cards.forEach(i => {
         if (i == item.start) {
-           return createEvent(i);
+            console.log(item);
+           createEvent(item);
         }
     })
 })
 
+
+
 function createEvent(item) {
-    pageEvent.innerHTML += `
-    <div class="card" id="${item}">
-    <div class="card__time" >
-        <div class="card__text">5:00</div>
-    </div>
-    <div class="card__event">
-    <p>New text<p></p></div>
-</div>
-    `
+    let card__event = document.querySelector('.card__event');
+    card__event.innerHTML = `
+    <p>${item.title}</p>
+   `
 }
