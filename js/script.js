ce8201a2ad939bc9d572e8e2aaaff88ff19ec40c
Harik Ovsianyk <ovsyanik.garik@gmail.com>
2021-09-12 13:54:52 +0300

    N2 compare arrays

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+let arrayEvents = [
+    {start: 0, duration:15, title: "Exercise"},
+    {start: 25, duration:15, title: "Exercise"},
+    {start: 30, duration:15, title: "Exercise"},
+    {start: 60, duration:15, title: "Exercise"},
+    {start: 100, duration:15, title: "Exercise"},
+    {start: 180, duration:15, title: "Exercise"},
+    {start: 360, duration:15, title: "Exercise"},
+    {start: 370, duration:15, title: "Exercise"},
+    {start: 405, duration:15, title: "Exercise"},
+];
+
+const timeSlot = document.querySelectorAll('.card');
+let pageEvent = document.querySelector('.card');
+let cards = Array.from(timeSlot).map(item => item.id);
+
+arrayEvents.map(item => {
+    cards.map(i => {
+        if (i == item.start) {
+           return createEvent(i);
+        }
+    })
+})
+
+function createEvent(item) {
+    pageEvent.innerHTML += `
+    <div class="card" id="${item}">
+    <div class="card__time" >
+        <div class="card__text">5:00</div>
+    </div>
+    <div class="card__event">
+    <p>New text<p></p></div>
+</div>
+    `
+}
